fcf55f33a7817aa3b944cb859c81b33ca30ce023
Víctor Berga <vbergae@gmail.com>
2011-08-08 23:38:36 +0200

    Added more tests to OutputStreamTests

--- BinaryStreamTests/OutputStreamTests.m
+++ BinaryStreamTests/OutputStreamTests.m
@@ -8,13 +8,15 @@
 
 #import "OutputStreamTests.h"
 
+#define setTestFile(file)     [_testPath stringByAppendingPathComponent:file]
+
 @implementation OutputStreamTests
 
 - (void)setUp
 {
     [super setUp];
     
-    _testPath = @"/Users/vbergae/test.bin";
+    _testPath = @"/Users/vbergae/Desktop/tests";
     _outputStream = [[OutputStream alloc] init];
     STAssertNotNil(_outputStream, @"Failed instantiatin OutputStream");
 }
@@ -32,9 +34,12 @@
 
 - (void)testSetPath
 {    
-    [_outputStream setPath:_testPath];
-    STAssertTrue([_outputStream.path isEqualToString:_testPath], 
-                 @"Path property broken");
+    NSString *path = setTestFile(@"prop.dat");
+    
+    [_outputStream setPath:path];
+    STAssertTrue([_outputStream.path isEqualToString:path], 
+                 @"Path should be %@, but is %@",
+                 path, _outputStream.path);
 }
 
 - (void)testIsOpen
@@ -48,7 +53,7 @@
 
 - (void)testOpenCloseStream
 {
-    [_outputStream setPath:_testPath];
+    [_outputStream setPath:setTestFile(@"open_close.dat")];
     
     // Opening file
     STAssertNoThrow([_outputStream openStream], @"openStream fails");
@@ -62,32 +67,50 @@
 
 - (void)testWriteChar
 {
-    [_outputStream setPath:_testPath];
+    [_outputStream setPath:setTestFile(@"char.dat")];
     [_outputStream openStream];
     
-    STAssertNoThrow([_outputStream writeChar:0xFF], @"FAIL");
+    STAssertNoThrow([_outputStream writeChar:127], @"FAIL");
+    STAssertNoThrow([_outputStream writeChar:-1], @"FAIL");
+    STAssertNoThrow([_outputStream writeChar:255], @"FAIL");
     
     [_outputStream closeStream];
 }
 
 - (void)testWriteUnsignedChar
 {
-    [_outputStream setPath:_testPath];
+    [_outputStream setPath:setTestFile(@"uchar.dat")];
     [_outputStream openStream];
     
-    STAssertNoThrow([_outputStream writeUnsignedChar:0xFF], @"FAIL");
+    STAssertNoThrow([_outputStream writeUnsignedChar:10], @"FAIL");
+    STAssertNoThrow([_outputStream writeChar:127], @"FAIL");
+    STAssertNoThrow([_outputStream writeChar:128], @"FAIL");    
+    STAssertNoThrow([_outputStream writeChar:-1], @"FAIL");
+    STAssertNoThrow([_outputStream writeChar:255], @"FAIL");
     
     [_outputStream closeStream];    
 }
 
 - (void)testWriteShort
 {
-    [_outputStream setPath:_testPath];
+    [_outputStream setPath:setTestFile(@"short.dat")];
     [_outputStream openStream];
     
-    STAssertNoThrow([_outputStream writeUnsignedChar:0xFFFF], @"FAIL");
+    STAssertNoThrow([_outputStream writeShort:0xFFFF], @"FAIL");
     
     [_outputStream closeStream];    
+}
+
+- (void)testWriteString
+{
+    [_outputStream setPath:setTestFile(@"string.dat")];
+    [_outputStream openStream];
+    
+    NSString *cadena = @"Hola esto es una prueba";
+    
+    STAssertNoThrow([_outputStream writeString:cadena], @"FAIL");
+    
+    [_outputStream closeStream];
 }
 
 @end
